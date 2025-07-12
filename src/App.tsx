fbb1e760f2ee34ecd547901ae8b0d2731e37c790
Fistreet-aman <aman.mathur@fistreet>
2025-07-12 19:19:38 +0530

    feature: Dropdown feature in Furniture

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 // src/App.tsx
-import CollectionGrid from "./components/CollectionGrid";
-import Footer from "./components/Footer";
-import Hero from "./components/Heroslider";
+import {Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Hero from "./components/Heroslider";
+import CollectionGrid from "./components/CollectionGrid";
 import ProductGrid from "./components/ProductGrid";
 import ExploreWoodsSection from "./components/WoodSection";
+import Footer from "./components/Footer";
+import Collection from "./context/Collections";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Hero />
-      <div>
-        <CollectionGrid />
-      </div>
-        <ProductGrid />
-        <ExploreWoodsSection />
-        <Footer />
-    </>
+      <Routes>
+        {/* Homepage */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <CollectionGrid />
+              <ProductGrid />
+              <ExploreWoodsSection />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Dynamic Collection Page */}
+        <Route path="/collection/:name" element={<Collection />} />
+      </Routes>
+      </>
+    
   );
 };
 
